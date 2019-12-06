af5b3f8074b3162ee5778bef3dd498d4deb2b49f
Bram Fouts <43616716+abfouts@users.noreply.github.com>
2019-12-05 17:06:39 -0800

    SnoopInval.c

---
+++ bram/snoopInval.c
@@ -0,0 +1,20 @@
+int snoopInval(int address, cLine cache[][SET_ASS])
+{
+    int way;
+    int index;
+    
+    uint32_t tempTag = makeMask(21, 32) & address;
+    tempTag = tempTag >> 20;
+    way = findMatch(getIndex(address), tempTag, cache);
+    
+    if (cache[index][way-1].mesi == 'M'){
+        cache[index][way-1].mesi = 'I';
+        //BusOperation() Maybe call BUS OPERATION HERE HITM
+        return HITM;
+    } else if (cache[index][way-1].mesi != 'I') {
+        cache[index][way-1].mesi = 'I';
+        return HIT;
+    } else {
+        return NOHIT;
+    }
+};
